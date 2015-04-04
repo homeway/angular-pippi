7d13a5c071cf6858c200767648889e33a23ad932
homeway <homeway.xue@gmail.com>
2015-04-05 06:17:54 +0800

    new wsProvider

--- websocket.js
+++ websocket.js
@@ -1,147 +1,149 @@
 angular.module('pippi.websocket', [])
 
-.provider('ws', function() {
-    var dataMap = {};
-    var msgQueue = [];
-    var callQueue = [];
-    var callSeq = 0;
+.provider('ws', {
+    websocket: null,
+    wsServer: null,
+    eventMap: {},
+    msgQueue: [],
+    callQueue: [],
+    callSeq: 0,
 
-    var fire = function(eventType, event) {
-      // console.log(dataMap);
-      // console.log(eventType + ":" + event.data);
-      if (dataMap[eventType]) {
-        for (var i = 0; i < dataMap[eventType].length; i++) {
-          dataMap[eventType][i](event);
+    fire: function(eventType, event) {
+      console.log(this.eventMap);
+      console.log(eventType + ":" + event.data);
+      if (this.eventMap[eventType]) {
+        for (var i = 0; i < this.eventMap[eventType].length; i++) {
+          this.eventMap[eventType][i](event);
         }
       }
-    };
+    },
 
-    var call_func = function(Seq, data) {
-      // console.log("seq: " + Seq, data);
-      // console.log(callQueue);
+    call_func: function(Seq, data) {
+      console.log("seq: " + Seq, data);
+      console.log(this.callQueue);
 
       TempQueue = [];
-      for(var i=0; i < callQueue.length; i++) {
-        Item = callQueue.pop();
+      for(var i=0; i < this.callQueue.length; i++) {
+        Item = this.callQueue.pop();
         if(Item.seq == Seq){
           Item.func(data);
-          callQueue.concat(TempQueue);
+          this.callQueue.concat(TempQueue);
           break;
         }
         else {
-          TempQueue.push(callQueue[i]);
+          TempQueue.push(this.callQueue[i]);
         }
       }
-      // console.log(callQueue);
-    };
+      console.log(this.callQueue);
+    },
 
-    var add_to = function(eventType, handler) {
+    add_to: function(eventType, handler) {
       //multiple event listener
-      if (!dataMap[eventType]) {
-        dataMap[eventType] = [];
+      if (!this.eventMap[eventType]) {
+        this.eventMap[eventType] = [];
       };
-      dataMap[eventType].push(handler);
-      // console.log(dataMap);
-    };
+      this.eventMap[eventType].push(handler);
+      console.log(eventMap);
+    },
 
-    var is_onnected = function() {
-      if (dataMap.websocket) {
-        return dataMap.websocket.readyState ==  dataMap.websocket.OPEN;
+    is_onnected: function() {
+      if (this.websocket) {
+        return this.websocket.readyState ==  this.websocket.OPEN;
       }
       else {
         return false
       }      
-    };
+    },
 
-    var confirm_connect = function() {
-      if(!is_onnected() && dataMap.wsServer) {
-        var websocket = new WebSocket(dataMap.wsServer);
-        websocket.onopen = function(evt) {
-          while(msgQueue.length > 0) {
-            websocket.send(msgQueue.pop())
+    confirm_connect: function(self) {
+      if(!self.is_onnected() && self.wsServer) {
+        var ws = new WebSocket(self.wsServer);
+        ws.onopen = function(evt) {
+          while(self.msgQueue.length > 0) {
+            ws.send(self.msgQueue.pop())
           };
-          fire("onOpen", evt)
+          self.fire("onOpen", evt)
         };
-        websocket.onclose   = function(evt) { fire("onClose", evt) };
-        websocket.onerror   = function(evt) { fire("onError", evt) };
-        websocket.onmessage = function(evt) {
+        ws.onclose   = function(evt) { self.fire("onClose", evt) };
+        ws.onerror   = function(evt) { self.fire("onError", evt) };
+        ws.onmessage = function(evt) {
           var Res = JSON.parse(evt.data);
           if(Res.length == 3 && Res[0] == 'call_resp')  {
-            // console.log(Res);
-            call_func(Res[1], Res[2]);
+            console.log(Res);
+            self.call_func(Res[1], Res[2]);
           }
           else {
             if(Res.length > 1) {
-              fire("onMessage."+Res[0], {data: Res.slice(1, Res.length)})
+              self.fire("onMessage."+Res[0], {data: Res.slice(1, Res.length)})
             }
             else {
-              fire("onMessage", evt)
+              self.fire("onMessage", evt)
             }
           }
         };
-        dataMap['websocket']  = websocket;
+        self.websocket  = ws;
       }
-    };
+    },
 
-    var Methods = {
-      connect : function() {
-        confirm_connect();
-      },
-      reconnect : function(wsServer) {
-        if (dataMap.websocket) {
-          dataMap.websocket.close();
-        };
-        connect(wsServer);
-      },
-      close : function() {
-        if (dataMap.websocket) {
-          dataMap.websocket.close();
-        }
-      },
-
-      onOpen    : function(handler) { add_to("onOpen", handler) },
-      onClose   : function(handler) { add_to("onClose", handler) },
-      onError   : function(handler) { add_to("onError", handler) },
-      onMessage : function(Cmd, handler) { add_to("onMessage."+Cmd, handler) },
-
-      send : function(Msg) {
-        if(is_onnected()) {
-          dataMap.websocket.send(Msg);
-        }
-        else {
-          msgQueue.push(Msg);
-        }
-        confirm_connect();
-      },
-
-      call : function(Cmd, Func) {
-        callSeq++,
-        callQueue.push({'seq': callSeq, 'func': Func});
-        Command = JSON.stringify(['call', callSeq, Cmd]);
-        if(is_onnected()) {
-          dataMap.websocket.send(Command);
-        }
-        else {
-          msgQueue.push(Command);
-        }
-        confirm_connect();
-      },
-
-      isConnect : function() { is_onnected() },
-
-      clean : function() {
-        if (dataMap.websocket) {
-          dataMap.websocket.close();
-        }
-        dataMap = {};
-      }
-    };
-
-    setWebsocketServer: function(path) {
-      dataMap['wsServer'] = path;
-    };
+    setServer: function(path) {
+      this.wsServer = path;
+    },
 
     $get: function() {
+      var self = this;
+      var Methods = {
+        connect : function() {
+          self.confirm_connect(self);
+        },
+        reconnect : function(wsServer) {
+          if (self.websocket) {
+            self.websocket.close();
+          };
+          connect(wsServer);
+        },
+        close : function() {
+          if (self.websocket) {
+            self.websocket.close();
+          }
+        },
+
+        onOpen    : function(handler) { this.add_to("onOpen", handler) },
+        onClose   : function(handler) { this.add_to("onClose", handler) },
+        onError   : function(handler) { this.add_to("onError", handler) },
+        onMessage : function(Cmd, handler) { this.add_to("onMessage."+Cmd, handler) },
+
+        send : function(Msg) {
+          if(self.is_onnected()) {
+            self.websocket.send(Msg);
+          }
+          else {
+            self.msgQueue.push(Msg);
+          }
+          self.confirm_connect(self);
+        },
+
+        call : function(Cmd, Func) {
+          self.callSeq++,
+          self.callQueue.push({'seq': self.callSeq, 'func': Func});
+          Command = JSON.stringify(['call', self.callSeq, Cmd]);
+          if(self.is_onnected()) {
+            self.websocket.send(Command);
+          }
+          else {
+            self.msgQueue.push(Command);
+          }
+          self.confirm_connect(self);
+        },
+
+        isConnect : function() { self.is_onnected() },
+
+        clean : function() {
+          if (self.websocket) {
+            self.websocket.close();
+          }
+          self.eventMap = {};
+        }
+      };
       return Methods;
     }
 });
